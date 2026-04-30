0606ae806cf650c6227e2ef63fd2936836f78c4a
ebelleon <24588023+eberureon@users.noreply.github.com>
2026-04-30 15:38:03 +0200

    change space indention for space

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -4,43 +4,43 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://ebelleon.de",
-	integrations: [sitemap()],
-	i18n: {
-		defaultLocale: "de",
-		locales: ["de", "en"],
-		routing: {
-			prefixDefaultLocale: false,
-		},
-	},
-	server: {
-		headers: {
-			"Content-Security-Policy":
-				"default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' 'unsafe-hashes';",
-		},
-	},
-	build: {
-		// Enable CSS inlining for smaller files to reduce requests
-		inlineStylesheets: "auto",
-		// Optimize assets
-		assets: "_astro",
-	},
-	// For production builds
-	vite: {
-		server: {
-			headers: {
-				"Content-Security-Policy": "default-src 'self'; script-src 'self'",
-			},
-		},
-		build: {
-			// Enable CSS code splitting
-			cssCodeSplit: true,
-			// Optimize chunks
-			rollupOptions: {
-				output: {
-					manualChunks: undefined,
-				},
-			},
-		},
-	},
+  site: "https://ebelleon.de",
+  integrations: [sitemap()],
+  i18n: {
+    defaultLocale: "de",
+    locales: ["de", "en"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+  server: {
+    headers: {
+      "Content-Security-Policy":
+        "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' 'unsafe-hashes';",
+    },
+  },
+  build: {
+    // Enable CSS inlining for smaller files to reduce requests
+    inlineStylesheets: "auto",
+    // Optimize assets
+    assets: "_astro",
+  },
+  // For production builds
+  vite: {
+    server: {
+      headers: {
+        "Content-Security-Policy": "default-src 'self'; script-src 'self'",
+      },
+    },
+    build: {
+      // Enable CSS code splitting
+      cssCodeSplit: true,
+      // Optimize chunks
+      rollupOptions: {
+        output: {
+          manualChunks: undefined,
+        },
+      },
+    },
+  },
 });
